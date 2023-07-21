b07006496c8a97aeda91745ca71e58914e68c456
MatteoGunetto <Matteogunetto@tiscali.it>
2023-07-21 16:03:39 +0200

    add campi

--- database/migrations/2023_07_21_133357_create_songs_table.php
+++ database/migrations/2023_07_21_133357_create_songs_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table -> id();
 
             $table -> string("title");
+            $table -> text("lyrics");
+            $table -> text("thumb");
 
             $table -> timestamps();
         });
