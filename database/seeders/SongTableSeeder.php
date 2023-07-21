8724f467a00d7251b35f50dcf91fbe9bf4fc4a14
Luca Cassano <lucacassano96@gmail.com>
2023-07-21 16:32:22 +0200

    creazione faker

--- database/seeders/SongTableSeeder.php
+++ database/seeders/SongTableSeeder.php
@@ -4,18 +4,17 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Song;
 
-class DatabaseSeeder extends Seeder
+class SongTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this -> call([
-            SongTableSeeder :: class
-        ]);
+        Song :: factory() -> count(10) -> create();
     }
 }
