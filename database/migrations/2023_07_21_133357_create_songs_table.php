4a4a409a2a983914829b9f7af0bffeebf602a66a
Sula Michele <sulamichele95@gmail.com>
2023-07-21 15:52:53 +0200

    Added song_length to table

--- database/migrations/2023_07_21_133357_create_songs_table.php
+++ database/migrations/2023_07_21_133357_create_songs_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table -> id();
 
             $table -> string("title");
+            $table->time('song_length');
 
             $table -> timestamps();
         });
